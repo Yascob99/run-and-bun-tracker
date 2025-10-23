7441cd36b5245a26076768ae929493049baae273
Yascob99 <Yascob99@users.noreply.github.com>
2025-10-22 23:03:38 -0600

    Delete run-and-bun-tracker/webserver directory
    don't need it due to new approach.

--- run-and-bun-tracker/webserver/start.bat
+++
@@ -1,2 +0,0 @@
-start /d ".\run-and-bun-tracker/webserver/" redbean-RBT.com -vv -d -L redbean.log -P redbean.pid
-echo "redbean started on port 8080 in daemon mode. Log file: redbean.log, PID file: redbean.pid"
